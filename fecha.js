9d5a329196f8c35b037c661fc038015e8b113c5f
JoseGabino12 <jmorales18@ucol.mx>
2020-02-18 22:46:04 -0600

    Tercer commit

--- fecha.js
+++ fecha.js
@@ -1,3 +1,4 @@
+var global
 export default class Fecha{
     /**
      * 
@@ -5,8 +6,9 @@ export default class Fecha{
      * @param {number} mes 
      * @param {number} año 
      */
+    
     constructor(dia, mes, año){
-        this.fechas = new Date(año,mes -1,dia)
+        this.fechas = new Date( año, mes - 1, dia)
         this.diasSemana = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"]
         this.años = ["Enero","Febrero","Marzo", "Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
         console.log("Objeto inicializado")
@@ -14,19 +16,31 @@ export default class Fecha{
     getAños(){
         let añosTranscurridos 
         añosTranscurridos = 2020 - this.fechas.getFullYear() 
+        global = añosTranscurridos
         return `${añosTranscurridos}`
     }
     getMeses(){
-
+        let mesesTranscurridos
+        mesesTranscurridos = global * 12
+        global = mesesTranscurridos
+        return `${mesesTranscurridos}`
     }
     getSemanas(){
-
+        let semanasTranscurridas 
+        semanasTranscurridas = global * 4
+        global = semanasTranscurridas
+        return `${semanasTranscurridas}`
     }
     getDias(){
-
+        let diasTrasncurridos
+        diasTrasncurridos = global * 7
+        global = diasTrasncurridos
+        return `${diasTrasncurridos}`
     }
     getFecha(){
-
+        let mesDelAño
+        mesDelAño = this.años[this.fechas.getMonth()]
+        return `${this.fechas.getDate()}/${mesDelAño}/${this.fechas.getFullYear()}`
     }
     getDiaFecha(){
 
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import Fecha from "./fecha.js"
 class Main{
     constructor(){
          this.horas = new Tiempo(11,30,"PM")
-         this.fechas = new Fecha(30,2,2001)
+         this.fechas = new Fecha(30,4,2001)
 
     }
     getTempo(){
@@ -14,6 +14,11 @@ class Main{
     }
     getFecha(){
         console.log(`Año ${this.fechas.getAños()}`)
+        console.log(`Meses ${this.fechas.getMeses()}`)
+        console.log(`Semanas ${this.fechas.getSemanas()}`)
+        console.log(`Días ${this.fechas.getDias()}`)
+        console.log(`Fecha ${this.fechas.getFecha()}`)
+
     }
 }
 let app = new Main()
